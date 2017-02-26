8238061fb56caf53947a22444600f513cf2abdb0
followmsi <followmsi@gmail.com>
2017-02-26 21:52:48 +0100

    TWRP configurations
    Squashed commits from followmsi device tree
    twrp settings

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -133,3 +133,15 @@ USE_CLANG_BIONIC := false
 # enable test relocations needed for closed camera libs
 TARGET_NEEDS_TEXT_RELOCATIONS := true
 
+#TWRP config:
+DEVICE_RESOLUTION := 1200x1920
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_CRYPTO := true
+TW_CUSTOM_POWER_BUTTON := 107
+TW_BRIGHTNESS_PATH := /sys/devices/platform/msm_fb.591617/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_NO_SCREEN_BLANK := true
+TW_CUSTOM_BATTERY_PATH := /sys/devices/i2c-0/0-0055/power_supply/battery
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone0/temp
+TW_INCLUDE_MTP := true
